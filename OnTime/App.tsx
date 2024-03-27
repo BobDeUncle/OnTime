4b9017aaacb350a8f3bd34b54c5944ef7cc47ff1
BobDeUncle <jessemansbridge@hotmail.com>
2024-03-27 17:09:13 +1100

    OneSignal Simple example
    Added in OneSignal Initialisation with simple example

--- OnTime/App.tsx
+++ OnTime/App.tsx
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {useWindowDimensions} from 'react-native';
+import { LogLevel, OneSignal } from 'react-native-onesignal';
 
 import HomeScreen from './screens/HomeScreen.js';
 import SettingsScreen from './screens/SettingsScreen.js';
@@ -27,6 +28,22 @@ function SideDrawer() {
 }
 
 function App(): React.JSX.Element {
+  // OneSignal for Push Notifications
+  // Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  // OneSignal Initialization
+  OneSignal.initialize('62fe518c-74b6-48ed-9973-37a81eb1d4b8');
+
+  // requestPermission will show the native iOS or Android notification permission prompt.
+  // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.Notifications.requestPermission(true);
+
+  // Method for listening for notification clicks
+  OneSignal.Notifications.addEventListener('click', event => {
+    console.log('OneSignal: notification clicked:', event);
+  });
+
   return (
     <NavigationContainer>
       <SideDrawer />
