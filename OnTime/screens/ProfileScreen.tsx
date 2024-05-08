7876d46cd46e850492074543eb6ae33b73be4e2f
BobDeUncle <jessemansbridge@hotmail.com>
2024-05-08 13:07:43 +1000

    Update ProfileScreen.tsx Styling

--- OnTime/screens/ProfileScreen.tsx
+++ OnTime/screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {useTheme} from '../theme/Colors';
 import styles from '../theme/Styles';
 import APIClient from '../api/APIClient';
@@ -28,11 +28,42 @@ function ProfileScreen(): React.ReactElement {
     fetchUser();
   }, []);
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 24,
+      backgroundColor: colors.background,
+    },
+    headingText: {
+      color: colors.opText,
+      fontSize: 24,
+      paddingBottom: 10,
+      fontWeight: 'bold',
+    },
+    text: {
+      color: colors.opText,
+      paddingBottom: 10,
+    },
+    boldText: {
+      fontWeight: 'bold',
+    },
+  });
+
   return (
-    <View style={{backgroundColor: colors.background}}>
-      <MyText style={{color: colors.opText}}>Name: {user ? user.firstName + ' ' + user.lastName : ''}</MyText>
-      <MyText style={{color: colors.opText}}>Role: {user && user.roles.length > 0 ? user.roles[0].name : ''}</MyText>
-      <MyText style={{color: colors.opText}}>Email: {user ? user.email : ''}</MyText>
+    <View style={styles.container}>
+      <MyText style={styles.headingText}>User Info</MyText>
+      <MyText style={styles.text}>
+        <MyText style={styles.boldText}>Name:</MyText>
+        {user ? ` ${user.firstName} ${user.lastName}` : ''}
+      </MyText>
+      <MyText style={styles.text}>
+        <MyText style={styles.boldText}>Role:</MyText>
+        {user && user.roles.length > 0 ? ` ${user.roles[0].name}` : ''}
+      </MyText>
+      <MyText style={styles.text}>
+        <MyText style={styles.boldText}>Email:</MyText>
+        {user ? ` ${user.email}` : ''}
+      </MyText>
     </View>
   );
 }
