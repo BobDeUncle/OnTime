112494e9a5337802e72c57f19c84cb97206267b1
BobDeUncle <jessemansbridge@hotmail.com>
2024-05-27 21:55:17 +1000

    Update edit-user-form.tsx
    Check for existing roles on edit user and removed password on edit user.

--- OnTime/components/user/edit-user-form.tsx
+++ OnTime/components/user/edit-user-form.tsx
@@ -30,11 +30,15 @@ const EditUserForm: React.FC<UserFormProps> = ({ user, styles, showCloseButton, 
   const lastNameRef = useRef<TextInput>(null);
   const [email, setEmail] = useState(user?.email || '');
   const emailRef = useRef<TextInput>(null);
-  const [password, setPassword] = useState(user?.password || '');
-  const passwordRef = useRef<TextInput>(null);
-  const [isAdminActive, setIsAdminActive] = useState(false);
-  const [isEmployeeActive, setIsEmployeeActive] = useState(true);
-  const [isSupervisorActive, setIsSupervisorActive] = useState(false);
+  const [isAdminActive, setIsAdminActive] = useState(
+    user?.roles.some(role => role.name === 'admin') || false
+  );
+  const [isEmployeeActive, setIsEmployeeActive] = useState(
+    user?.roles.some(role => role.name === 'employee') || false
+  );
+  const [isSupervisorActive, setIsSupervisorActive] = useState(
+    user?.roles.some(role => role.name === 'supervisor') || false
+  );
 
   const roleData = [
     {
@@ -83,16 +87,14 @@ const EditUserForm: React.FC<UserFormProps> = ({ user, styles, showCloseButton, 
     const isFirstNameValid = firstName.trim().length > 0;
     const isLastNameValid = lastName.trim().length > 0;
     const isEmailValid = validateEmail(email);
-    const isPasswordValid = password.trim().length > 0;
 
     // Update validation status
     setFirstNameValid(isFirstNameValid);
     setLastNameValid(isLastNameValid);
     setEmailValid(isEmailValid);
-    setPasswordValid(isPasswordValid);
 
     // If any input is invalid, return early
-    if (!isFirstNameValid || !isLastNameValid || !isEmailValid || !isPasswordValid) {
+    if (!isFirstNameValid || !isLastNameValid || !isEmailValid) {
       setLoading(false);
       return;
     }
@@ -100,7 +102,7 @@ const EditUserForm: React.FC<UserFormProps> = ({ user, styles, showCloseButton, 
     // Show a confirmation popup
     Alert.alert(
       'Submit Updated User',
-      `Do you wish to update the user ${firstName} ${lastName} with email as ${email}, password as ${password} and role(s) of ${getSelectedRoles().map(role => role.name).join(", ")}?`,
+      `Do you wish to update the user ${firstName} ${lastName} with email as ${email} and role(s) of ${getSelectedRoles().map(role => role.name).join(", ")}?`,
       [
         {
           text: 'Cancel',
@@ -115,7 +117,6 @@ const EditUserForm: React.FC<UserFormProps> = ({ user, styles, showCloseButton, 
                 firstName,
                 lastName,
                 email,
-                password,
                 getSelectedRoles,
               });
 
@@ -123,7 +124,6 @@ const EditUserForm: React.FC<UserFormProps> = ({ user, styles, showCloseButton, 
                 firstName,
                 lastName,
                 email,
-                password,
                 getSelectedRoles,
               });
         
@@ -310,31 +310,10 @@ const EditUserForm: React.FC<UserFormProps> = ({ user, styles, showCloseButton, 
           setEmailValid(validateEmail(text));
         }}
         returnKeyType="next"
-        onSubmitEditing={() => passwordRef.current?.focus()}
-        blurOnSubmit={false}
       />
       {!emailValid && (
         <MyText style={localStyles.invalidForm}>
           <FontAwesomeIcon icon='exclamation' style={localStyles.invalidFormIcon}/> Invalid Email
-        </MyText>
-      )}
-      <TextInput
-        ref={passwordRef}
-        style={{
-          ...localStyles.textInput,
-          borderColor: passwordValid ? colors.border : colors.warning,
-        }}
-        placeholder="Password"
-        placeholderTextColor={localStyles.placeholderText.color}
-        value={password}
-        onChangeText={(text) => {
-          setPassword(text);
-          setPasswordValid(text.trim().length > 0);
-        }}
-      />
-      {!passwordValid && (
-        <MyText style={localStyles.invalidForm}>
-          <FontAwesomeIcon icon='exclamation' style={localStyles.invalidFormIcon}/> Invalid Password
         </MyText>
       )}
       <View style={localStyles.roleContainer}>
