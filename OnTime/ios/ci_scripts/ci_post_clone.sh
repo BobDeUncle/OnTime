031cf27e6c8286aee4d632fcfe038dc9d38d76d3
BobDeUncle <jessemansbridge@hotmail.com>
2024-05-30 17:49:09 +1000

    Update ci_post_clone.sh

--- OnTime/ios/ci_scripts/ci_post_clone.sh
+++ OnTime/ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# This script is run after the repository is cloned
 
 # Install Homebrew if not installed
 if ! command -v brew &> /dev/null
@@ -8,12 +7,19 @@ then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# Install Node.js
+if ! command -v node &> /dev/null
+then
+    echo "Node.js not found. Installing Node.js..."
+    brew install node
+fi
+
 # Install CocoaPods using Homebrew
 echo "Installing CocoaPods..."
 brew install cocoapods
 
 # Navigate to the project directory
-cd ios
+cd /Volumes/workspace/repository/OnTime/ios
 
 # Install the pods
 echo "Running pod install..."
