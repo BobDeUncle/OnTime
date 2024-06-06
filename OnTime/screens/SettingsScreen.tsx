c46f564bb36b5eca172d7320c015c1e1f1261596
BobDeUncle <jessemansbridge@hotmail.com>
2024-06-06 13:20:55 +1000

    Settings Screen Update

--- OnTime/screens/SettingsScreen.tsx
+++ OnTime/screens/SettingsScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View, Switch} from 'react-native';
-import {useTheme} from '.././theme/Colors';
+import { View, Switch, StyleSheet } from 'react-native';
+import { useTheme } from '.././theme/Colors';
 import styles from '.././theme/Styles';
 import MyText from '../components/MyText';
 
@@ -13,13 +13,33 @@ function SettingsScreen({
   isDarkMode,
   toggleTheme,
 }: SideDrawerProps): React.ReactElement {
-  const {colors} = useTheme();
+  const { colors } = useTheme();
+
+  const localStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+      padding: 16,
+    },
+    card: {
+      backgroundColor: colors.card,
+      padding: 16,
+      borderRadius: 8,
+      marginVertical: 8,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    text: {
+      color: colors.text,
+      fontSize: 16,
+    },
+  });
 
   return (
-    <View style={{backgroundColor: colors.background}}>
-      <MyText style={{color: colors.opText}}>This is Settings Screen</MyText>
-      <View style={styles.leftContainer}>
-        <MyText style={{color: colors.opText}}>Dark Mode</MyText>
+    <View style={localStyles.container}>
+      <View style={localStyles.card}>
+        <MyText style={localStyles.text}>Dark Mode</MyText>
         <Switch value={isDarkMode} onValueChange={toggleTheme} />
       </View>
     </View>
